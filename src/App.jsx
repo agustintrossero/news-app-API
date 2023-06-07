b9931cba24933a62d507d375fe3d73c9daa953f8
Agustin Trossero <96441753+agustintrossero@users.noreply.github.com>
2023-06-07 18:52:46 +0200

    Creation of the Good News App project with Vite, axios, MUI, the code that comes by default was cleaned

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+//import Container from '@mui/material/Container';
+import {Container, Typography} from '@mui/material'
+
+function App() {
+
+
+  return (
+  <>
+    <Container>
+      <header>
+        <Typography  align='center' marginY={5} component="h1" variant="h3">Good News App</Typography>
+      </header>
+    </Container>
+  </>
+  )
+}
+
+export default App
