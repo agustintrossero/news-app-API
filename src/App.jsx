476e3a7df300eee09bf6f715403dcc1ff13f80a2
Agustin Trossero <96441753+agustintrossero@users.noreply.github.com>
2023-06-09 17:10:21 +0200

    creating the necessary provider structure to be able to consult the news API

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,31 @@
 //import Container from '@mui/material/Container';
 import {Container, Typography, Grid} from '@mui/material'
 import Formulario from './components/Formulario'
+import { NoticiasProvider } from './context/NoticiasProvider'
 
 function App() {
 
 
   return (
   <>
-    <Container>
-      <header>
-        <Typography  align='center' marginY={5} component="h1" variant="h3">Good News App</Typography>
-      </header>
+    <NoticiasProvider>
+      <Container>
+        <header>
+          <Typography  align='center' marginY={5} component="h1" variant="h3">Good News App</Typography>
+        </header>
 
-        <Grid
-          container
-          direction='row'
-          justifyContent='center'
-          alignItems='center'
-        >
-          <Grid item md={6} xs={12} lg={4}>
-            <Formulario/>
+          <Grid
+            container
+            direction='row'
+            justifyContent='center'
+            alignItems='center'
+          >
+            <Grid item md={6} xs={12} lg={4}>
+              <Formulario/>
+            </Grid>
           </Grid>
-        </Grid>
-    </Container>
+      </Container>
+    </NoticiasProvider>
   </>
   )
 }
