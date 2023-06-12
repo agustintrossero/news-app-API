9fe9b30fbce084dd26a48a7d72349edb38bdad7c
Agustin Trossero <96441753+agustintrossero@users.noreply.github.com>
2023-06-12 18:43:27 +0200

    using useEffect to query the news api and so by assigning a state to news to be able to consume it in a component

--- src/context/NoticiasProvider.jsx
+++ src/context/NoticiasProvider.jsx
@@ -1,12 +1,24 @@
-import { useState, createContext } from "react";
+import { useState, useEffect,createContext } from "react";
+import axios from "axios";
+
 const NoticiasContext = createContext();
 
 function NoticiasProvider({ children }) {
   const [categoria, setCategoria] = useState("general");
+  const [noticias, setNoticias] = useState([]);
+
+  useEffect(()=>{
+    const consultarAPI = async()=>{
+        const url = `https://newsapi.org/v2/top-headlines?country=ar&category=${categoria}&pageSize=100&apiKey=${import.meta.env.VITE_API_KEY}`
+        const {data} = await axios(url)
+        setNoticias(data.articles)
+    }
+    consultarAPI()
+  },[categoria])
+
 
   const handleChangeCategoria = (e) => {
     setCategoria(e.target.value);
-    console.log(e.target.value);
   };
   return (
     <NoticiasContext.Provider
